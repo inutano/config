68ed7df49f8b83b0279950feb6298d59e93c3869
Tazro Inutano Ohta <inutano@gmail.com>
2016-12-15 12:10:17 +0900

    setup script updated

--- ec2-init.sh
+++ ec2-init.sh
@@ -3,7 +3,14 @@
 #
 
 # update yum and install packages
-sudo yum update -y && sudo yum install -y curl gcc zsh git nano tmux nginx
+sudo yum update -y && sudo yum install -y curl gcc openssl-devel readline-devel zlib-devel zsh git nano tmux nginx
+
+# setup timezone
+echo -e 'ZONE="Asia/Tokyo"\nUTC=false' | sudo tee /etc/sysconfig/clock
+sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+# setup language
+echo "LANG=ja_JP.UTF-8" | sudo tee /etc/sysconfig/i18n
 
 # setup dotfiles
 git clone https://github.com/inutano/config.git $HOME/.config
@@ -24,3 +31,44 @@ git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-buil
 rbenv install 2.3.0
 rbenv rehash
 gem install bundler
+
+
+# interactive setup
+
+cat << EOS
+Automatic configuration done. Your next step is:
+
+- setup root
+
+$ sudo useradd <admin name>
+$ sudo su <admin name>
+$ echo <ssh-publickey> $HOME/.ssh/authorized_keys
+$ sudo visudo
+
+Defaults timestamp_timeout = 3
+<admin name>  ALL = (ALL) NOPASSWD: ALL, !/bin/su
+
+$ echo 'SU_WHEEL_ONLY yes' | sudo tee -a /etc/login.defs
+$ echo 'auth required /lib64/security/pam_wheel.so use_uid' | sudo tee -a /etc/pam.d/su
+
+- erase ec2-user
+
+$ sudo userdel -r ec2-user
+
+- ssh login setup
+
+$ sudo nano /etc/ssh/sshd_config
+
+PermitRootLogin No
+PasswordAuthentication no
+
+- setup aws command
+
+$ aws configure
+
+AWS Access Key ID [None]: XXXXXXXXXXXXXXXXXXXX
+AWS Secret Access Key [None]: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+Default region name [None]: ap-northeast-1
+Default output format [None]: 
+
+EOS
